13c244bc21ec83ca2c159b9080148e0d226432cc
Hermes Caretta <hermescaretta@gmail.com>
2021-03-08 11:17:30 -0300

    changed something that i don't remember what it was

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement, sign_in } from './store/actions';
-import { Provider } from 'react-redux';
-import store from './store/store';
 
 function App() {
   const counter = useSelector((state) => state.counter);
@@ -9,15 +7,13 @@ function App() {
   const dispatch = useDispatch();
 
   return (
-    <Provider store={store}>
-      <div className="App">
-        <h1>Counter: {counter}</h1>
-        <button onClick={() => dispatch(increment(5))}>+</button>
-        <button onClick={() => dispatch(decrement(4))}>-</button>
-        <button onClick={() => dispatch(sign_in())}>Login</button>
-        {isLogged ? <h3>Valuable information i shoudn't see</h3> : ''}
-      </div>
-    </Provider>
+    <div className="App">
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(4))}>-</button>
+      <button onClick={() => dispatch(sign_in())}>Login</button>
+      {isLogged ? <h3>Valuable information i shoudn't see</h3> : ''}
+    </div>
   );
 }
 
